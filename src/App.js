e1c5081dea45fb2d253ba86388b7e36f21fd75d6
Diana Nazarova <alllleksandrova@mail.ru>
2024-02-24 12:54:12 +0300

    feat: add router

--- src/App.js
+++ src/App.js
@@ -1,15 +1,27 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
 import Header from "./components/header/Header";
 import Home from "./pages/Home";
 import Vacancy from "./pages/Vacancy";
 import Contacts from "./pages/Contacts";
+import Job from "./pages/Job";
 
 function App() {
   return (
     <div className="App">
-      <Header />
-      {/* <Home /> */}
-      <Vacancy />
-      {/* <Contacts /> */}
+      <Router>
+        <Header />
+
+
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/vacancy" element={<Vacancy />}/>
+            <Route path="/job" element={<Job />}/>
+            <Route path="/contacts" element={<Contacts />}/>
+          </Routes>
+
+
+      </Router>
     </div>
   );
 }
---
+++ src/pages/Job.jsx
@@ -0,0 +1,11 @@
+import Info from "../components/info/Info";
+
+const Job = () => {
+	return (
+		<main>
+			<Info />
+		</main>
+	);
+}
+
+export default Job;
